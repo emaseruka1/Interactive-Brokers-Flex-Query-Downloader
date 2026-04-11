db3e72e7ce1c949e7560dff9cdd3d93338739665
emaseruka1 <emmamaseruka97@gmail.com>
2026-04-12 01:17:16 +0200

    refactored code

--- src/main/java/com/example/portfolio/controller/RunJob.java
+++ src/main/java/com/example/portfolio/controller/RunJob.java
@@ -3,7 +3,6 @@ package com.example.portfolio.controller;
 import com.example.portfolio.service.GetFlexXmlStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RunJob {
 
-    @Autowired
-    private GetFlexXmlStatement getFlexXmlStatement;
+    private final GetFlexXmlStatement getFlexXmlStatement;
+
+    public RunJob(GetFlexXmlStatement getFlexXmlStatement) {
+        this.getFlexXmlStatement = getFlexXmlStatement;
+    }
 
     private static final Logger log = LoggerFactory.getLogger(RunJob.class);
 
