492480c73a998c626653f8354bc29122dd549b3d
emaseruka1 <emmamaseruka97@gmail.com>
2026-04-12 00:37:02 +0200

    modify run logic

--- src/main/java/com/example/portfolio/controller/RunJob.java
+++ src/main/java/com/example/portfolio/controller/RunJob.java
@@ -3,6 +3,7 @@ package com.example.portfolio.controller;
 import com.example.portfolio.service.GetFlexXmlStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RunJob {
 
+    @Autowired
     private GetFlexXmlStatement getFlexXmlStatement;
 
     private static final Logger log = LoggerFactory.getLogger(RunJob.class);
